fba4d53eb24517149bba2c4b2dd39aad562a6651
Alex <fugent89@yandex.ru>
2021-07-05 13:50:10 +0300

    Lecture8StreamAPI_Lambda остальные примеры

--- src/main/java/lecture8StreamAPI_Lambda/StreamExample2.java
+++ src/main/java/lecture8StreamAPI_Lambda/StreamExample2.java
@@ -1,19 +1,36 @@
 package lecture8StreamAPI_Lambda;
 
+import com.sun.jdi.request.StepRequest;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class StreamExample2 {
     public static void main(String[] args) {
        // secondEx();
        // thirdEx();
        //matchEx();
-        findAnyEx();
-
+//        findAnyEx();
+//        mappingEx();
+//        reduceEx();
+//        intStreamEx();
+//        streamCreation();
+//        streamFromFile();
+//        simpleStringEx();
+        uniqueWords();
     }
 
     private static void secondEx() {
@@ -52,8 +69,89 @@ public class StreamExample2 {
             System.out.println(opt.get());
         }
     }
-}
 
-/**
- * 1,24,30
- */
+    private static void mappingEx() {
+        Function<String, Integer> _strToLen = String::length;
+        Function<String, Integer> strToLen = s-> s.length();
+        Predicate<Integer> evenNumberFilter = n -> n % 5 == 0;
+        Function<Integer, Integer> cube = n -> n * n * n;
+        Stream.of(1,2,3).map(n -> Math.pow(n, 3));
+
+        List<String> list = Arrays.asList("A", "BB", "C", "DDD", "EE", "FFFF");
+        List<Integer> wordsLength = list.stream().map(str -> str.length()).collect(Collectors.toList());
+//        List<Integer> wordsLength = list.stream().map(strToLen).collect(Collectors.toList());
+
+        System.out.println(list);
+        System.out.println(wordsLength);
+
+        list.stream().map(_strToLen).forEach(n -> System.out.print(n));
+        list.stream().map(_strToLen).forEach(System.out::println);
+    }
+
+    private static void reduceEx() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        int sum = 0;
+        for (Integer o:list) {
+            sum += o;
+        }
+        int streamSum = list.stream().reduce(0, (a, b) -> a + b);
+        System.out.println(sum + " " + streamSum);
+    }
+
+    private static void intStreamEx() {
+        IntStream myIntStream = IntStream.of(10,20,30,40,50);
+
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        list.stream().mapToInt(v -> v).sum();
+
+        IntStream.rangeClosed(2,10).filter(n -> n % 2 == 0).forEach(System.out::println);
+
+    }
+
+    private static void streamCreation() {
+        Arrays.asList("A", "B", "C").stream().forEach(System.out::println);
+        Stream.of(1,2,3,4).forEach(System.out::println);
+        Arrays.stream(new int[]{4,3,2,1}).forEach(System.out::println);
+    }
+
+    private static void streamFromFile() {
+        try {
+            Files.lines(Paths.get("123.txt")).map(String::length).forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void simpleStringEx(){
+        System.out.println(Arrays.stream("A B CC B C AA A A B CC C".split("\\s")).distinct().count());
+    }
+
+    private static void uniqueWords() {
+        try {
+            Stream.of("A", "B", "A", "C", "A", "B").distinct().forEach(System.out::println);
+
+//            Files.lines(Paths.get("WarAndPeace.txt"))
+//                    .map(line -> line.split("\\s"))
+//                    .distinct()
+//                    .forEach(arr -> System.out.println(Arrays.toString(arr)));
+//            System.out.println("****************************");
+
+//            Files.lines(Paths.get("WarAndPeace.txt"))
+//                    .map(line -> line.split("\\s"))
+//                    .map(Arrays::stream)
+//                    .distinct()
+//                    .forEach(System.out::println);
+//            System.out.println("****************************");
+//
+            System.out.println(Files.lines(Paths.get("WarAndPeace.txt"))
+                    .map(line -> line.split("\\s"))
+                    .flatMap(Arrays::stream)
+                    .distinct()
+                    .collect(Collectors.joining(", ", "Уникальные слова: ", ".")));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
